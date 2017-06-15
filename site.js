12f84bd099a211e7bf6cc5c2014a9dfe8f4f8c6e
inexorableace <rik.saunders@icloud.com>
2017-06-15 19:45:01 -0400

    added work section to index

--- site.js
+++ site.js
@@ -1,28 +1,6 @@
 $('.scroll').on('click', function(){
-	$('html, body').animate({scrollTop: $('.work').offset().top}, 500);
+	event.preventDefault();
+	$('html, body').animate({scrollTop: $('#work').offset().top}, 300);
 });
 
- $(window).scroll(function(){
-	 var topVal = $('.work').offset().top - 45;
-if (document.documentElement.scrollTop || document.body.scrollTop >= topVal){
-	if ($(window).width() <= 420){
-		$('nav').css('bottom', '0px')
-		$('nav').css('top', 'auto')
-	}else{
-		$('nav').css('top', '0px')
-		$('nav').css('bottom', 'auto')
-	}
-	
-	}
-else{
-	if ($(window).width() <= 420){
-		$('nav').css('bottom', '-45px')
-		$('nav').css('top', 'auto')
-	}else{
-		$('nav').css('top', '-45px')
-		$('nav').css('bottom', 'auto')
-	}
-	
-	}
-	
-}); 
+
