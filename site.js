5f7715931b6d84417bd9f565b15cf5035de000a0
inexorableace <rik.saunders@icloud.com>
2017-06-14 20:43:00 -0400

    updates to index, added menu

--- site.js
+++ site.js
@@ -1,27 +1,28 @@
-$(document).ready(function () {
-    var logoFront = document.getElementById('logoFront');
-    var logoBack = document.getElementById('logoBack');
-    logoFront.homePos = { x: logoFront.offsetLeft, y: logoFront.offsetTop };
-    logoBack.homePos = { x: logoBack.offsetLeft, y: logoBack.offsetTop };
-    
-    $('.header').mousemove(function (e) {
-        parallax(e, logoFront, 11);
-        parallax(e, logoBack, 14);
-    });
-});
-
-$(window).resize(function () {
-	$(".logo").attr("style",""); 
-	logoFront.homePos = { x: logoFront.offsetLeft, y: logoFront.offsetTop };
-    logoBack.homePos = { x: logoBack.offsetLeft, y: logoBack.offsetTop };
-});
-
-function parallax(e, target, layer) {
-    var x = target.homePos.x - (e.pageX - target.homePos.x) / layer;
-    var y = target.homePos.y - (e.pageY - target.homePos.y) / layer;
-    $(target).offset({ top: y ,left : x });
-};
-
 $('.scroll').on('click', function(){
 	$('html, body').animate({scrollTop: $('.work').offset().top}, 500);
 });
+
+ $(window).scroll(function(){
+	 var topVal = $('.work').offset().top - 45;
+if (document.documentElement.scrollTop || document.body.scrollTop >= topVal){
+	if ($(window).width() <= 420){
+		$('nav').css('bottom', '0px')
+		$('nav').css('top', 'auto')
+	}else{
+		$('nav').css('top', '0px')
+		$('nav').css('bottom', 'auto')
+	}
+	
+	}
+else{
+	if ($(window).width() <= 420){
+		$('nav').css('bottom', '-45px')
+		$('nav').css('top', 'auto')
+	}else{
+		$('nav').css('top', '-45px')
+		$('nav').css('bottom', 'auto')
+	}
+	
+	}
+	
+}); 
