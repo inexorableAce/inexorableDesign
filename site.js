1f7ba93a8d2257c79daea1a8caa46f12cb354290
inexorableace <rik.saunders@icloud.com>
2019-04-20 19:32:05 -0400

    link fixes

--- site.js
+++ site.js
@@ -7,20 +7,29 @@ window.onscroll = function () {
     }
 };
 
+window.touchmove = function () { 
+    if (document.documentElement.scrollTop >= 200 ) {
+        document.getElementById('root').classList.add("changeColor");
+    } 
+    else {
+        document.getElementById('root').classList.remove("changeColor");
+    }
+};
 
-var navLinks = document.querySelectorAll('nav a');
 
-for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener('click', smoothScroll)
-} 
+// var navLinks = document.querySelectorAll('nav a');
 
-function smoothScroll(e){
-    e.preventDefault();
-    let item = document.querySelector(this.getAttribute("href"));
-    let wrapper = document.documentElement
-    let count = item.offsetTop - wrapper.scrollTop - 60 // xx = any extra distance from top ex. 60
-    wrapper.scrollBy({top: count, left: 0, behavior: 'smooth'})
-}
+// for (let i = 0; i < navLinks.length; i++) {
+//     navLinks[i].addEventListener('click', smoothScroll)
+// } 
+
+// function smoothScroll(e){
+//     e.preventDefault();
+//     let item = document.querySelector(this.getAttribute("href"));
+//     let wrapper = document.documentElement
+//     let count = item.offsetTop - wrapper.scrollTop - 60 // xx = any extra distance from top ex. 60
+//     wrapper.scrollBy({top: count, left: 0, behavior: 'smooth'})
+// }
 
 
 
