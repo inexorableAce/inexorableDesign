f3e07bd3b8d5ed5dea06632d1134a235d86f7975
inexorableace <rik.saunders@icloud.com>
2019-04-22 20:41:56 -0400

    header change added smooth scroll

--- site.js
+++ site.js
@@ -1,27 +1,14 @@
-// window.onscroll = function () {
-//     if (window.scrollY >= 200 ) {
-//         document.getElementById('root').classList.add("changeColor");
-//     } 
-//     else {
-//         document.getElementById('root').classList.remove("changeColor");
-//     }
-// };
+var navLinks = document.querySelectorAll('.scrollLink');
 
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', smoothScroll)
+} 
 
-
-// var navLinks = document.querySelectorAll('nav a');
-
-// for (let i = 0; i < navLinks.length; i++) {
-//     navLinks[i].addEventListener('click', smoothScroll)
-// } 
-
-// function smoothScroll(e){
-//     e.preventDefault();
-//     let item = document.querySelector(this.getAttribute("href"));
-//     let wrapper = document.documentElement
-//     let count = item.offsetTop - wrapper.scrollTop - 60 // xx = any extra distance from top ex. 60
-//     wrapper.scrollBy({top: count, left: 0, behavior: 'smooth'})
-// }
+function smoothScroll(e){
+    e.preventDefault();
+    let item = this.getAttribute("href");
+    document.querySelector(item).scrollIntoView({ behavior: 'smooth' });
+}
 
 
 
